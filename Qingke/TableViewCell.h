717ba7fd0ddbe83b5f917812b07a0784b26eb862
Leo Zhang <leo_zhang@cityenglish.org>
2016-09-13 10:35:04 +0800

    添加Cell

---
+++ Qingke/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  Qingke
+//
+//  Created by 张晓争 on 9/13/16.
+//  Copyright © 2016 CityEnglish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ Qingke/TableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.m
+//  Qingke
+//
+//  Created by 张晓争 on 9/13/16.
+//  Copyright © 2016 CityEnglish. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
